34af2cd48f5ba10c2a503f987efe81dc4c68ee8d
Ibramelias <ibramdarwish@gmail.com>
2021-01-14 12:36:37 -0500

    created schema to save new data

--- fitness.js
+++ fitness.js
@@ -1,3 +1,50 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const exercises = new Schema({
+    type: {
+        type: Option,
+
+
+    },
+
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    distance: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+
+    duration: {
+        type: Number,
+        required: true
+    },
+
+
+    weight: {
+        type: Number,
+        required:true
+    },
+
+    sets: {
+        type: Number,
+        required: true
+    },
+
+    reps: {
+        type: Number,
+        required: true
+    }
+
+
+
+
+
+
+
+})
